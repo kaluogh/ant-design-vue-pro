a67dca606931e6aac5d301aa52377b9f7a0bd946
kaluogh <zhangwenxin6615006@163.com>
2022-03-23 13:43:32 +0800

    Update util.js

--- src/utils/util.js
+++ src/utils/util.js
@@ -93,3 +93,23 @@ export function scorePassword (pass) {
 
   return parseInt(score)
 }
+
+// custom add
+/**
+ * 过滤对象中为空的属性
+ * @param obj
+ * @returns {*}
+ */
+ export function filterObj(obj) {
+	if (!(typeof obj == 'object')) {
+	  return;
+	}
+  
+	for ( let key in obj) {
+	  if (obj.hasOwnProperty(key)
+		&& (obj[key] == null || obj[key] == undefined || obj[key] === '')) {
+		delete obj[key];
+	  }
+	}
+	return obj;
+  }
